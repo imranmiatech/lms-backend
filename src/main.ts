f62326c492ee1f0a46168ca44c1d46e8794ba494
imransoftvence <imranmia@softvence.com>
2026-06-29 13:28:16 +0600

    add euro

--- src/main.ts
+++ src/main.ts
@@ -45,7 +45,7 @@ async function bootstrap() {
   const port = process.env.PORT ?? 3000;
 
   // Enable CORS for local frontend, Swagger, and VS Code Live Server.'https://braens.vercel.app'
-  const frontend = process.env.FRONTEND_URL || 'https://braens.eu' ||'http://localhost:5173' || 'https://braens.vercel.app';
+  const frontend = process.env.FRONTEND_URL ||'http://localhost:5173' || 'https://braens.vercel.app';
   const configuredOrigins = frontend.includes(',')
     ? frontend.split(',').map((s) => s.trim())
     : [frontend.trim()];
@@ -54,11 +54,7 @@ async function bootstrap() {
       ...configuredOrigins,
       'https://braens.vercel.app',
       'http://localhost:5173',
-      'https://braens.eu',
-      'https://onset-depletion-vanity.ngrok-free.dev',
-      'http://127.0.0.1:5500',
-      'http://localhost:5501',
-      'http://127.0.0.1:5501',
+      // 'https://braens.eu'
     ]),
   );
 
